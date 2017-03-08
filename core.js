78264a050a1708696a40f649c17fa37a0770d962
LBYPatrick <lbypatrick.public@gmail.com>
2017-03-07 20:07:58 -0500

    Revert "Minor Bug Fixes"
    This reverts commit c5919b49d76d8871457d85ebf664d95f148ade15.

--- core.js
+++ core.js
@@ -4,7 +4,7 @@
 (function(){
     console.log("Don't be evil!");
     const actualCode = "Object.defineProperty(navigator,'platform',{get:function(){return '这里随便吧';}});";
-    let s = document.createElement('script');
+    const s = document.createElement('script');
     s.textContent = actualCode;
     document.documentElement.appendChild(s);
 s.remove();
